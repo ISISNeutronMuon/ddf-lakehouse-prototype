b2d2600831f21cbeb635bf08028a6fb2f77294c7
Martyn Gigg <martyn.gigg@gmail.com>
2024-07-18 12:20:26 +0100

    Split Spark into a separate image.
    With Jupyter in the same container its easy for the
    secrets for Iceberg to be revealed. In practice we
    would likely want a separate Jupyter instance anyway
    and this gives a chance to try Spark Connect as a way
    of connecting remotely to a spark cluster.
    
    A separate Jupyter setup will follow.

--- ansible/roles/spark/files/entrypoint.sh
+++ ansible/roles/spark/files/entrypoint.sh
@@ -2,11 +2,4 @@
 #
 
 start-master.sh -p 7077
-start-worker.sh spark://127.0.0.1:7077
-start-history-server.sh
-start-thriftserver.sh  --driver-java-options "-Dderby.system.home=/tmp/derby"
-
-# Entrypoint, for example notebook, pyspark or spark-sql
-if [[ $# -gt 0 ]] ; then
-    eval "$1"
-fi
+SPARK_NO_DAEMONIZE=true start-connect-server.sh --packages org.apache.spark:spark-connect_2.12:3.5.1
