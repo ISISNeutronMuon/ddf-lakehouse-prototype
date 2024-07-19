ac5cbf605f94c3d2cbbb2cb3b61c0b43049b7db3
Martyn Gigg <martyn.gigg@gmail.com>
2024-07-19 13:53:57 +0100

    Disable raw file descriptor output in notebooks
    Spark Connect is very chatty and the output will be
    meaningless to users

---
+++ ansible/roles/jupyter/files/ipython/ipython_kernel_config.py
@@ -0,0 +1,4 @@
+# See https://ipython.readthedocs.io/en/stable/config/intro.html#setting-configurable-options
+
+# Suppress low-level file-descriptor output from extensions
+c.IPKernelApp.capture_fd_output = False
