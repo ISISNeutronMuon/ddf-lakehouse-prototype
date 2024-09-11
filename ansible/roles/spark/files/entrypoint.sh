483e16b281655e3cef9f72d63277203431485215
Martyn Gigg <martyn.gigg@gmail.com>
2024-09-11 10:20:29 +0100

    Use maven to install additional jar files for Spark
    A base layer in the docker image downloads what is needed
    and then we copy them into the final image. This sorts out
    the dependency chain properly.

--- ansible/roles/spark/files/entrypoint.sh
+++ ansible/roles/spark/files/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #
+SPARK_CONNECT_VERSION=3.5.1
 
 start-master.sh -p 7077
-SPARK_NO_DAEMONIZE=true start-connect-server.sh --packages org.apache.spark:spark-connect_2.12:3.5.1
+SPARK_NO_DAEMONIZE=true start-connect-server.sh --packages org.apache.spark:spark-connect_${SCALA_VERSION}:${SPARK_CONNECT_VERSION}
