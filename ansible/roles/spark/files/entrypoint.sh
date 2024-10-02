48f45da7f14d4ebbe6103538791865d0119902aa
Martyn Gigg <martyn.gigg@gmail.com>
2024-10-02 17:10:50 +0100

    Remove Spark Connect and configure PySpark with spark-defaults
    Note that the secrets are not protected at all one the playbook
    has run!

--- ansible/roles/spark/files/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-SPARK_CONNECT_VERSION=3.5.1
-
-start-master.sh -p 7077
-SPARK_NO_DAEMONIZE=true start-connect-server.sh --packages org.apache.spark:spark-connect_${SCALA_VERSION}:${SPARK_CONNECT_VERSION}
