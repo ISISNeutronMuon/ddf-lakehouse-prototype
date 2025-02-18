0670601eb8b7c337c597aa51f8599b0ab55b4bf2
Martyn Gigg <martyn.gigg@gmail.com>
2025-02-18 17:08:55 +0000

    Make iceberg.get_or_create_catalog threadsafe

--- pipelines/pipelines-common/src/pipelines_common/utils/iceberg.py
+++ pipelines/pipelines-common/src/pipelines_common/utils/iceberg.py
@@ -1,22 +1,32 @@
+from pathlib import Path
+import threading
 from typing import Dict, Optional
 
-from pyiceberg.typedef import Identifier
 from pyiceberg.catalog.rest import Catalog, RestCatalog
 from pyiceberg.catalog.sql import SqlCatalog
+from pyiceberg.typedef import Identifier
+
 
 _catalog: Optional[Catalog] = None
+_catalog_lock = threading.Lock()
 
 
-# Iceberg
 def catalog_get_or_create(catalog_properties: Dict[str, str]) -> Catalog:
     """Connect to an iceberg catalog on a given uri"""
-    global _catalog
-    if _catalog is None:
-        uri = catalog_properties["uri"]
-        if uri.startswith("sqlite:///"):
-            _catalog = SqlCatalog(**catalog_properties)
-        else:
-            _catalog = RestCatalog(**catalog_properties)
+    global _catalog, _catalog_lock
+    if _catalog is not None:
+        return _catalog
+
+    with _catalog_lock as lock:
+        if _catalog is None:
+            uri = catalog_properties["uri"]
+            if uri.startswith("sqlite:///"):
+                init_catalog_tables = "false" if Path(uri[10:]).exists() else "true"
+                _catalog = SqlCatalog(
+                    init_catalog_tables=init_catalog_tables, **catalog_properties
+                )
+            else:
+                _catalog = RestCatalog(**catalog_properties)
 
     return _catalog
 
