6426f4f1befdf50c2c27c1b32aa33459f66029d4
Martyn Gigg <martyn.gigg@gmail.com>
2025-04-10 11:59:01 +0100

    Remove requirement of docs build to provide source directory.

--- docs/build.sh
+++ docs/build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 # Assume uv is available.
 # Create a local virtual environment, install build requirements and build
-# into a `build` subdirectory of the project root
 set -euo pipefail
 
 if [ $# = 0 ]; then
-  echo "Usage: $0 <project_root> <build_dir> [...other arguments passed to mkdocs build]"
+  echo "Usage: $0 <build_dir> [...other arguments passed to mkdocs build]"
   exit 1
 fi
 
@@ -15,13 +14,14 @@ if [ -n "${VIRTUAL_ENV:-}" ]; then
 fi
 
 # Constants
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 VENV_DIR=.venv
 DEFAULT_BUILD_ARGS="--clean"
 
 # Argument handling
-project_root=$1
-build_dir=$2
-shift 2
+build_dir=$1
+shift 1
+project_root=$SCRIPT_DIR
 user_build_args=$*
 
 # Create and configure environment
