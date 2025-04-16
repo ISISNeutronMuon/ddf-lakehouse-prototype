b20ff0d436ab6c39d96f7fb54f2122011b447e6b
Martyn Gigg <martyn.gigg@gmail.com>
2025-04-16 16:50:02 +0100

    Add cron script for statusdisplay api ingest.

---
+++ pipelines/ingest/statusdisplay/cron-scripts/daily.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Script configuration
+set -euo pipefail
+
+# Variables required for this script
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+EXTRACT_AND_LOAD_SCRIPT=extract_and_load.py
+LOG_LEVEL=DEBUG
+ON_PIPELINE_FAILURE=log_and_continue
+REQUIREMENTS_TXT=requirements.txt
+
+# Create & configure environment
+pushd $SCRIPT_DIR/..
+test -d .venv || uv venv
+uv pip install -r $REQUIREMENTS_TXT
+
+# Execute
+uv run $EXTRACT_AND_LOAD_SCRIPT --log-level $LOG_LEVEL --on-pipeline-step-failure $ON_PIPELINE_FAILURE
