58c75bba5643e80fca2676ded351497cbc6e4d04
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2025-05-15 11:20:37 +0100

    fix(pipelines-common): Trim trailing slashes from pyiceberg bucket_url config parameter (#52)
    ### Summary
    
    Using the `pyiceberg` destination it was possible to create a table
    using a location that had empty path components, .e.g.
    `s3://mybucket//dataset_name/table_name` by using a `bucket_url` with a
    trailing slash (`s3://mybucket/` in this example). This would raise an
    error when the files were accessed in the object storage.
    
    This changes trims trailing slashes in the config parameter.
    
    *There is no associated issue.*

--- pipelines/pipelines-common/src/pipelines_common/dlt_destinations/pyiceberg/configuration.py
+++ pipelines/pipelines-common/src/pipelines_common/dlt_destinations/pyiceberg/configuration.py
@@ -1,10 +1,8 @@
 import dataclasses
-from typing import Any, Dict, Literal, Final, Optional, TypeAlias
+from typing import Dict, Literal, Final, Optional, TypeAlias
 
 from dlt.common.configuration import configspec
-from dlt.common.configuration.specs.base_configuration import (
-    CredentialsConfiguration,
-)
+from dlt.common.configuration.specs import CredentialsConfiguration
 from dlt.common.destination.client import DestinationClientDwhConfiguration
 from dlt.common.utils import digest128
 
@@ -28,7 +26,11 @@ class PyIcebergRestCatalogCredentials(CredentialsConfiguration):
         return properties
 
     def headers_as_properties(self) -> Dict[str, str]:
-        return {"header.X-Iceberg-Access-Delegation": self.access_delegation}
+        from pyiceberg.utils.properties import HEADER_PREFIX
+
+        return {
+            f"{HEADER_PREFIX}X-Iceberg-Access-Delegation": self.access_delegation,
+        }
 
 
 PyIcebergCatalogCredentials: TypeAlias = PyIcebergRestCatalogCredentials
@@ -47,14 +49,21 @@ class IcebergClientConfiguration(DestinationClientDwhConfiguration):
     # and is intended to be expanded later
     table_location_layout: Optional[str] = r"{dataset_name}/{table_name}"
 
+    @property
+    def connection_properties(self) -> Dict[str, str]:
+        """Returns a mapping of connection properties to pass to the catalog constructor"""
+        return self.credentials.as_dict() if self.credentials is not None else {}
+
+    def on_resolved(self) -> None:
+        self.normalize_bucket_url()
+
+    def normalize_bucket_url(self) -> None:
+        """Normalizes bucket_url, i.e. removes any trailing slashes"""
+        self.bucket_url = self.bucket_url.rstrip("/")
+
     def fingerprint(self) -> str:
         """Returns a fingerprint of a connection string."""
 
         if self.credentials.uri:
             return digest128(self.credentials.uri)
         return ""
-
-    @property
-    def connection_properties(self) -> Dict[str, str]:
-        """Returns a mapping of connection properties to pass to the catalog constructor"""
-        return self.credentials.as_dict()
