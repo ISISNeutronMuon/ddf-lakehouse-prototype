dcee9672bbd989066544a6c1ffaf156887d1702f
Martyn Gigg <martyn.gigg@gmail.com>
2025-05-27 14:56:10 +0100

    Create a dbt staging model for statusdisplay
    The staging model can be used by other models while
    maintaining consistent table layout and naming schemes.

--- pipelines/modelling/isis/models/facility/cycles.sql
+++ pipelines/modelling/isis/models/facility/cycles.sql
@@ -1,78 +1,25 @@
-{# The schedule table defines pre- & post-cycle events but does not attribute #}
-{# them to a named cycle, e.g. #}
+with
 
-{# type        label            start                  end                  #}
-{# ------------------------------------------------------------------------ #}
-{# pre-cycle   Commissioning    2022-01-10 08:30:00    2022-02-21 08:30:00  #}
-{# pre-cycle   Run-up           2022-02-21 08:30:00    2022-03-01 08:30:00  #}
-{# cycle       2021/2           2022-03-01 08:30:00    2022-04-08 08:30:00  #}
-{# post-cycle  Machine physics  2022-04-08 08:30:00    2022-04-11 08:30:00  #}
+staging as (
 
-{# We want to label these date ranges as YYYY/NO also. #}
-{# This model assumes that a cycle is a contiguous block of dates where #}
-{# start(interval) = end(previous_interval) and uses windows functions to #}
-{# group each contiguous block and select the label where type='cycle' #}
-
-
-with schedule_grouped_by_date_block as (
     select
-        `type`,
-        `label`,
-        `start`,
-        `end`,
-        count(group_number) over (
-            order by
-                `start`
-        ) as group_number
+      *
     from
-        (
-            select
-                `type`,
-                `label`,
-                `start`,
-                `end`,
-                case
-                lag(`end`)
-                    over (
-                        order by
-                            `start`
-                    )
-                    when `start` then null
-                    else `row_number`
-                end as group_number
-            from
-                (
-                    select
-                        `type`,
-                        `label`,
-                        `start`,
-                        `end`,
-                        row_number() over (
-                            order by
-                                `start` asc
-                        ) as `row_number`
-                    from
-                        {{ source('sources__statusdisplay_api', 'schedule') }}
-                )
-        )
-),
+      {{ ref('stg_statusdisplay__schedule') }}
 
-renamed as (
-    select
-        t.`type`,
-        t.`label`,
-        t.`start` as started_at,
-        t.`end` as ended_at,
-        (
-            select first(g.label) as first_label
-            from
-                schedule_grouped_by_date_block as g
-            where
-                g.`type` = 'cycle'
-                and t.group_number = g.group_number
-        ) as cycle_name
-    from
-        schedule_grouped_by_date_block as t
 )
-
-select * from renamed
+select
+    l.`type`,
+    l.`label`,
+    l.`started_at`,
+    l.`ended_at`,
+    (
+        select first(r.`label`) as first_label
+        from
+          staging r
+        where
+            r.`type` = 'cycle'
+            and l.group_number = r.group_number
+    ) as `cycle_name`
+from
+    staging l
---
+++ pipelines/modelling/isis/models/staging/statusdisplay/stg_statusdisplay__schedule.sql
@@ -0,0 +1,67 @@
+{# The schedule table defines pre- & post-cycle events but does not assign #}
+{# an identifier to attribute them to the same "group" #}
+
+{# type        label            start                  end                  #}
+{# ------------------------------------------------------------------------ #}
+{# pre-cycle   Commissioning    2022-01-10 08:30:00    2022-02-21 08:30:00  #}
+{# pre-cycle   Run-up           2022-02-21 08:30:00    2022-03-01 08:30:00  #}
+{# cycle       2021/2           2022-03-01 08:30:00    2022-04-08 08:30:00  #}
+{# post-cycle  Machine physics  2022-04-08 08:30:00    2022-04-11 08:30:00  #}
+
+{# Here we assume a contiguous block of dates where start(interval) = end(previous_interval) #}
+{# and use windows functions to group each contiguous block. #}
+{# Each contiguous block is assigned a unique identifier #}
+
+with
+
+source as (
+
+  select * from {{ source('src_statusdisplay', 'schedule') }}
+
+),
+
+renamed as (
+
+  select
+    `type`,
+    `label`,
+    `start` AS started_at,
+    `end` AS ended_at,
+    count(group_number) over (
+        order by `start`
+    ) as group_number
+  from
+    (
+        select
+            `type`,
+            `label`,
+            `start`,
+            `end`,
+            case
+            lag(`end`)
+                over (
+                    order by
+                        `start`
+                )
+                when `start` then null
+                else `row_number`
+            end as group_number
+        from
+            (
+                select
+                    `type`,
+                    `label`,
+                    `start`,
+                    `end`,
+                    row_number() over (
+                        order by
+                            `start` asc
+                    ) as `row_number`
+                from
+                    source
+            )
+    )
+
+)
+
+select * from renamed
