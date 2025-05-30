71957aeb5b185d1a75562a7dbb679bc470bd2dc4
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2025-05-30 14:18:29 +0100

    Make table_location_layout optional in pyiceberg destination
    The catalog will create a UUID for the path components if no location is provided.
    This should discourage interaction with the S3 layer directly as it will not be so obvious
    what files belong to what table

--- pipelines/pipelines-common/tests/unit_tests/dlt_destinations/pyiceberg/test_configuration.py
+++ pipelines/pipelines-common/tests/unit_tests/dlt_destinations/pyiceberg/test_configuration.py
@@ -15,7 +15,7 @@ def test_initial_iceberg_config_state_defaults_to_rest():
 
     assert default_config.bucket_url is None
     assert default_config.catalog_type == "rest"
-    assert default_config.table_location_layout == r"{dataset_name}/{table_name}"
+    assert default_config.table_location_layout is None
     assert default_config.credentials is None
     assert default_config.connection_properties == {}
 
